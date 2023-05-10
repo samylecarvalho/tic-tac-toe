d7af8841f13292ac61234902b014a557f6633886
Samyle Carvalho <s4myle@gmail.com>
2023-05-10 12:17:56 -0400

    Create object About

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import logo from './logo.png';
-import './App.css';
+import About from './objects/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href='#' className="action">ABOUT</a>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
-      <div className='App-section'>
-          <p>TIC TAC TOE </p>
-      </div>
-    </div>
+    <About />
   );
 }
 
---
+++ src/objects/About/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './styles.css'
+
+const About = () => {
+    return (<a href='#to-do' className="about">ABOUT</a>)
+}
+
+export default About

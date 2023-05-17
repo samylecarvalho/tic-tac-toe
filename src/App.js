cdbe3069b4eef8fbe958c48b13f907a2fbb1641c
Samyle Carvalho <s4myle@gmail.com>
2023-05-16 20:08:26 -0400

    Create Player object without CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Board from './components/Board';
+
 
 function App() {
   return (
-      <Header />
+      <>
+        <Header />
+        <Board />
+      </>
   );
 }
 
 export default App;
-
-
-

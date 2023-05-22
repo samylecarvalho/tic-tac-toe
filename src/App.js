6f73241af17a7b8ad8dd710eb62913338c122385
Samyle Carvalho <s4myle@gmail.com>
2023-05-22 11:02:00 -0400

    Card object receives children parameter

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ function App() {
   return (
       <>
         <Header />
-        <Card />
-        <Player player='x' />
+        <Card> 
+          <Player player='x' />
+          <Player player='o' />
+          <Player player='o' />
+        </Card>
       </>
   );
 }

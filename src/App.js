1791e81fc8ff6837f2a4e01a971a6a3938142e36
Samyle Carvalho <s4myle@gmail.com>
2023-05-24 17:40:23 -0400

    Create Label object

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
+import Label from './objects/Label';
 
 const App = () => (
       <main className="app">
         <Header />
         <Hashtag />
+        <Label />
       </main>
   );
 
---
+++ src/objects/Label/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './style.css';
+
+const Label = () => (
+    <p className="label"> Mostrar eventos</p>    
+);
+
+export default Label;

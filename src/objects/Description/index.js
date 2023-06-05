46857fe5d67bc3f48bc7f3436743db71f9172f9f
Samyle Carvalho <s4myle@gmail.com>
2023-06-05 15:14:32 -0400

    Create Description object

---
+++ src/objects/Description/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const Description = () => (
+    <p className="description"> 
+        Migrei de palhaço para Dev. Front-End/UX e agora eu trabalho na comunidade da Impulso
+    </p>
+)
+
+export default Description;

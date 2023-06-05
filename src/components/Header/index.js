5cce949edea0a8eb928ea1a89029f00f448104aa
Samyle Carvalho <s4myle@gmail.com>
2023-06-05 19:36:10 -0400

    Change className on Header to improve usability

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./style.css";
+
 import Logo from "../../objects/Logo";
 import About from "../../objects/About";
 import Menu from "../../objects/Menu";
 
-const Header = () => (
+const Header = (className = "header") => (
     <header className="header">
         <Logo />
         <About />
         <Menu />
     </header>
-)
+);
 
 export default Header;
---
+++ src/objects/WhiteLogo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import LogoWhite from '../../images/white-logo.png';
+
+const WhiteLogo = () => (<img src={LogoWhite} alt='Logo CollabCode' className="white-logo" />)
+
+
+export default WhiteLogo;

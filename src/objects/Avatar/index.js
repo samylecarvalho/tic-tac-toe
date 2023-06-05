d105156814741f5f8967439917c0e937c2f17fff
Samyle Carvalho <s4myle@gmail.com>
2023-06-05 15:16:36 -0400

    Create Avatar object

---
+++ src/objects/Avatar/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+import AvatarMarco from "../../images/avatar.png" 
+
+const Avatar = () => (<img src={AvatarMarco} alt='Avatar Marco Bruno' className="avatar" />)
+
+export default Avatar;
+

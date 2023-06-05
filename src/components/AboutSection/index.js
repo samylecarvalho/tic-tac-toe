c4abce020e94715d9b3d43d956aaaaf684e9d769
Samyle Carvalho <s4myle@gmail.com>
2023-06-05 19:30:46 -0400

    Set width and height for AboutSection

---
+++ src/components/AboutSection/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Content from "../../objects/Content";
+import WhiteHeader from "../../objects/WhiteHeader";
+import "./style.css";
+
+const AboutSection = () => (
+    <article className="section -about">    
+        <WhiteHeader/>
+        <Content />
+    </article>
+)
+
+export default AboutSection;

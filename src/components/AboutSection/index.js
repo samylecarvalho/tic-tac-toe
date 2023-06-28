1374cfb28da18c3925e559dba9d4a06dae73c97d
Samyle Carvalho <s4myle@gmail.com>
2023-06-28 18:26:29 -0400

    Code refactoring on AboutSection

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import AboutLink from "../../objects/AboutLink";
 import Content from "../../objects/Content";
+import IconClose from "../../objects/IconClose";
 import "./style.css";
+
 
 const AboutSection = () => (
     <article className="section -about">    
+    <header className="header -white" >
+        <AboutLink className="-light" />
+        <IconClose />
+    </header>
         <Content />
+    
     </article>
 )
 

c04a3a86dfadf664900f0cc0f04b6bc52b46da20
Samyle Carvalho <s4myle@gmail.com>
2023-06-28 21:02:20 -0400

    Minor fixes on the design of the Header

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Logo from "../../objects/Logo";
 import AboutLink from "../../objects/AboutLink";
-import Content from "../../objects/Content";
 import IconClose from "../../objects/IconClose";
+import Content from "../../objects/Content";
 import "./style.css";
 
 
 const AboutSection = () => (
     <article className="section -about">    
     <header className="header -white" >
+        <Logo className="white-logo" />
         <AboutLink className="-light" />
         <IconClose />
     </header>

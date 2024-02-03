ebf666b01474035252a57214c93a2dbe353cfd91
Samyle Carvalho <s4myle@gmail.com>
2024-02-03 18:45:30 -0500

    Create IconSocial object

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import AvatarProfile from '../../objects/AvatarProfile';
 import Avatar from '../../images/avatar.png';
+import IconSocial from "../../objects/IconSocial";
 import "./style.css";
+import GitHub from '../../images/icon-github.png';
+import LinkedIn from '../../images/icon-linkedin.png';
+import Twitter from '../../images/icon-twitter.png';
+import Instagram from '../../images/icon-instagram.png';
 
 
 const ProfileUser = () => (
@@ -12,6 +17,12 @@ const ProfileUser = () => (
     <dt className="title">Samyle Carvalho</dt>
     <dd className="description">
       Brazilian based in Toronto. I have a BA in Psychology and am looking for my first Front-End/UX opportunity. Currently learning HTML, CSS JavaScript and React.
+    </dd>
+    <dd className="socials">
+      <IconSocial src={GitHub} alt="GitHub icon"/>
+      <IconSocial src={LinkedIn} alt="LinkedIn icon"/>
+      <IconSocial src={Twitter} alt="Twitter icon"/>
+      <IconSocial src={Instagram} alt="Instagram icon"/>
     </dd>
   </dl>
 );
